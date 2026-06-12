74e20554d4fe1b7dffa1b04955a83300345a7f32
Andito Dwi Wicaksono <anditodwi03@gmail.com>
2026-06-12 15:42:51 +0700

    feat: tambah routing API untuk endpoint POST /air

--- php-environment/routes/api.php
+++ php-environment/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EnvironmentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// POST dari Gateway (/iot/air -> /api/air)
+Route::post('/air', [EnvironmentController::class, 'store']);
